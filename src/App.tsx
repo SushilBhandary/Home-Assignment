780a40a25a69e64959ac1c15dfb7644847909ecd
SushilBhandary <sushil.bhandary.b@gmail.com>
2024-04-16 23:20:08 +0530

    adding filter compound

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,77 @@
 import React from 'react';
-import { AppBar, Box, Typography } from '@mui/material';
+import { AppBar, Box, Typography, Container , Grid, Button,Select, MenuItem,InputLabel, FormControl } from '@mui/material';
 import './App.css';
+import {makeStyles} from '@mui/material'
 
 function App() {
 
   return (
     <>
-      <AppBar sx={{ backgroundColor: '#352929', textAlign: 'center', padding: '5px' }}>
+      {/* <AppBar sx={{ backgroundColor: '#352929', textAlign: 'center', padding: '5px' }}>
         <Typography variant='h6'>Products Dashboard</Typography>
-      </AppBar>
+      </AppBar> */}
       <main>
-        <Box sx={{ display: 'flex', height: '100vh' }}> 
-          <Box sx={{ backgroundColor: 'lightblue', width: '30vw' }}>
-            <Box sx={{  }}>
+      <Grid container >
+        <Grid item xs={4} sx={{
+            backgroundColor: 'lightblue',
+            width: '30vw',
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+          <Box sx={{ backgroundColor: 'lightblue', width: '30vw', height:'80vh',borderStyle: 'solid', display: 'flex', flexDirection: 'column',  justifyContent: 'space-between',alignItems:'center', padding:"20px" }}>
+            <Container sx={{ width: '100%' }}>
+              <Grid container spacing={2}>
+                <Grid item  xs={6}>
+                  <h1>Filter</h1>
+                </Grid>
+                <Grid item  xs={6}>
+                  <Button variant="text">clear</Button>
+                </Grid>
+              </Grid>
               
-            </Box>
+              <FormControl variant="standard" sx={{ m: 1, width: '80%' }}>
+                <InputLabel id="demo-simple-select-standard-label">Select Category</InputLabel>
+                <Select
+                  labelId="demo-simple-select-standard-label"
+                  id="demo-simple-select-standard"
+                  label="Age"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+
+
+              <FormControl variant="filled" sx={{ m: 1, width: '80%'}}>
+                <InputLabel id="demo-simple-select-filled-label">Select Category</InputLabel>
+                <Select 
+                  labelId="demo-simple-select-filled-label"
+                  id="demo-simple-select-filled"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+            </Container>
+
+            <Button sx={{  width: '80%'}} variant="contained">Run Report</Button>
           </Box>
-          <Box sx={{ backgroundColor: 'lightcoral', width: '70vw' }}>
-            <h1>Right Side</h1>
-          </Box>
-        </Box>
+        </Grid>
+        <Grid item xs={8} sx={{ backgroundColor: 'lightcoral', width: '70vw', height:'100vh' }}>
+          {/* Replace 'xs=4' with appropriate content or remove this line */}
+          <h1>hi</h1>
+        </Grid>
+      </Grid>
       </main>
     </>
   );
