e615c17c5ca917ac77a225d3229bc510d25b2da6
SushilBhandary <sushil.bhandary.b@gmail.com>
2024-04-17 00:43:32 +0530

    adding functionaly on select caregory, product, clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,80 @@
-import React from 'react';
-import { AppBar, Box, Typography, Container , Grid, Button,Select, MenuItem,InputLabel, FormControl } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { AppBar, Checkbox, ListItemText, OutlinedInput, Box, Typography, Container , Grid, Button,Select, MenuItem,InputLabel, FormControl } from '@mui/material';
+import { SelectChangeEvent } from '@mui/material/Select';
+
 import './App.css';
 import {makeStyles} from '@mui/material'
+import axios from 'axios';
+
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+}
+
+interface ProductsResponse {
+  products: Product[];
+  total: number;
+  skip: number;
+  limit: number;
+}
 
 function App() {
+
+  const [categories, setCategories] = useState<string[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<string>('');
+  const [products, setProducts] = useState<Product[]>([]);
+  const [selectedProduct, setSelectedProduct] = useState<string[]>([]);
+
+  const fetchCategories = async() => {
+    await axios.get('https://dummyjson.com/products/categories')
+    .then(response => {
+      console.log(response.data);
+      setCategories([...response.data]);
+    })
+    .catch( e => console.log(e))
+  }
+
+  const fetchProduct = async(val: string) => {
+    await axios.get(`https://dummyjson.com/products/category/${val}`)
+    .then(response => {
+      console.log(response.data);
+      setProducts([...response.data.products]);
+    })
+    .catch( e => console.log(e))
+  }
+
+  const handleChangeOfcategories = (event: SelectChangeEvent<String>) => {
+    setSelectedCategory(event.target.value as string);
+    fetchProduct(event.target.value as string)
+  };
+
+  const handleChangeOfProduct = (event: SelectChangeEvent<typeof selectedProduct>) => {
+    const {
+      target: { value },
+    } = event;
+    setSelectedProduct(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
+  const handleChangeOfClean = () => {
+    setSelectedCategory('')
+    setSelectedProduct([])
+  }
+  useEffect( () => {
+    fetchCategories()
+  }, [])
+
 
   return (
     <>
@@ -27,39 +98,48 @@ function App() {
                   <h1>Filter</h1>
                 </Grid>
                 <Grid item  xs={6}>
-                  <Button variant="text">clear</Button>
+                  <Button variant="text" onClick={handleChangeOfClean}>clear</Button>
                 </Grid>
               </Grid>
               
               <FormControl variant="standard" sx={{ m: 1, width: '80%' }}>
                 <InputLabel id="demo-simple-select-standard-label">Select Category</InputLabel>
                 <Select
-                  labelId="demo-simple-select-standard-label"
-                  id="demo-simple-select-standard"
-                  label="Age"
+                  labelId="demo-multiple-name-label"
+                  id="demo-multiple-name"
+                  value={selectedCategory}
+                  onChange={handleChangeOfcategories}
+                  input={<OutlinedInput label="Name" />}
                 >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
+                  {categories.map((name) => (
+                    <MenuItem
+                      key={name}
+                      value={name}
+                    >
+                      {name}
+                    </MenuItem>
+                  ))}
                 </Select>
               </FormControl>
 
 
               <FormControl variant="filled" sx={{ m: 1, width: '80%'}}>
-                <InputLabel id="demo-simple-select-filled-label">Select Category</InputLabel>
-                <Select 
-                  labelId="demo-simple-select-filled-label"
-                  id="demo-simple-select-filled"
+                <InputLabel id="demo-simple-select-filled-label">Select Product</InputLabel>
+                <Select
+                  labelId="demo-multiple-checkbox-label"
+                  id="demo-multiple-checkbox"
+                  multiple
+                  value={selectedProduct}
+                  onChange={handleChangeOfProduct}
+                  input={<OutlinedInput label="Tag" />}
+                  renderValue={(selected) => selected.join(', ')}
                 >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
+                  {products.map((name) => (
+                    <MenuItem key={name.title} value={name.title}>
+                      <Checkbox checked={selectedProduct.indexOf(name.title) > -1} />
+                      <ListItemText primary={name.title} />
+                    </MenuItem>
+                  ))}
                 </Select>
               </FormControl>
             </Container>
@@ -78,3 +158,7 @@ function App() {
 }
 
 export default App;
+
+
+
+
